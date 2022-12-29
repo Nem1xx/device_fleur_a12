6a0ca3133464726a2a72f155bf9193a2d2b7133f
aghora7 <ukikieran@gmail.com>
2022-12-29 17:14:34 +0530

    fleur: build Thermal hal for mitigating overheat and control and let users know whats happening with heat management.
    Signed-off-by: aghora7 <ukikieran@gmail.com>

--- device.mk
+++ device.mk
@@ -156,5 +156,10 @@ PRODUCT_SHIPPING_API_LEVEL := 30
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@2.0.vendor
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/fleur/fleur-vendor.mk)
