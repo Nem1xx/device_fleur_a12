99e55303a040810877ac9d7e889329161c06439c
bibarub <bibarub@zohomail.eu>
2023-01-05 22:45:50 +0530

    fleur: build android.hardware.power@1.2.vendor

--- device.mk
+++ device.mk
@@ -128,7 +128,12 @@ PRODUCT_PACKAGES += \
     SystemUIOverlayViva \
     TelephonyOverlayViva \
 	CarrierConfigOverlayViva
-    
+
+# Power
+PRODUCT_PACKAGES += \
+    android.hardware.power@1.2.vendor \
+    android.hardware.power.stats@1.0-service.mock
+        
 # Product characteristics
 PRODUCT_CHARACTERISTICS := default
 
