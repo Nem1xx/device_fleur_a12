7776baba8775d2480023951c47139f106d30e541
aghora7 <ukikieran@gmail.com>
2023-02-18 06:50:25 +0530

    fleur: set VNDK version to 31 aka 12.1
    Signed-off-by: aghora7 <ukikieran@gmail.com>

--- device.mk
+++ device.mk
@@ -320,6 +320,12 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@2.0.vendor
 
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 31
+
+# Extra VNDK
+PRODUCT_EXTRA_VNDK_VERSIONS := 31
+
 # WiFi
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
